88a89ccad4f13ed708e501538ca1bef46602f0bb
nekoyade <ktr.km.sch@gmail.com>
2024-08-08 17:13:45 +0900

    refactor: server/settings.pyを修正

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import HelloWorld
+from django.urls import include, path
+from rest_framework import routers
+
+from api.views import HelloWorld
+
+
+router = routers.DefaultRouter()
+
 
 urlpatterns = [
     path('hello/', HelloWorld.as_view()),
+    path('', include(router.urls)),
 ]
