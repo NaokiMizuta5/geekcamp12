9f969c3f103e0c6de50ab20d5a998f851c274488
unknown <naoki.mizuta.work@gmail.com>
2024-08-08 20:03:32 +0900

    chore: addし忘れ

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from '@components/Navbar';
 import { Routes, Route } from 'react-router-dom';
-import Register from './pages/Register';
+import Register from '@pages/Register';
+import Navbar from '@components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
