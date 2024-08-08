377fac7bc34fe02265e88e2660a32aeace5f9beb
unknown <naoki.mizuta.work@gmail.com>
2024-08-08 20:59:13 +0900

    chore: 不必要な部分を削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Register from '@pages/Register';
 import Navbar from '@components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Router>
