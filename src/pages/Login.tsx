3517395a8eebe3fc86030f2ee492e985f9683508
smz-exe <shimizu-yuki13861@keio.jp>
2024-08-08 23:29:43 +0900

    Improve Login page UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../App.css';
+import logo from '../assets/logo1.png';
+
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState<string>('');
@@ -23,29 +26,38 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div className="container">
-            <h2>ログイン</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">メールアドレス:</label>
-                <input 
-                    type="email" 
-                    id="email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    required 
-                />
-                <label htmlFor="password">パスワード:</label>
-                <input 
-                    type="password" 
-                    id="password" 
-                    value={password} 
-                    onChange={(e) => setPassword(e.target.value)} 
-                    required 
-                />
-                <button type="submit">ログイン</button>
-            </form>
-            <button onClick={() => navigate('/register')}>ユーザー登録</button>
-        </div>
+        <>
+            <header className="header">
+            <div className="logo-container">
+                    <img src={logo} alt="Logo" className="logo-small" />
+                    <span className="app-name">Hablock</span>
+                </div>
+                <div className="welcome-text">Welcome back!</div>
+            </header>
+            <div className="container">
+                <h2>Login to Hablock</h2>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">Username:</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        required 
+                    />
+                    <label htmlFor="password">Password:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        required 
+                    />
+                    <button type="submit">Login</button>
+                </form>
+                <button className="register-button" onClick={() => navigate('/register')}>Sign up</button>
+            </div>
+        </>
     );
 };
 
