2f66bb52f63f80a595c2aa737af20bdee6b2e372
nekoyade <ktr.km.sch@gmail.com>
2024-08-09 00:10:26 +0900

    feature: UserSerializerにcreate(), update()を追加

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -7,6 +7,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'nickname',]
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # パスワードをハッシュ化して保存
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 
 class HabitItemSerializer(serializers.ModelSerializer):
