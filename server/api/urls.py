1317f4140270ce58cd8b0c91f516e6e0aaa44722
unknown <naoki.mizuta.work@gmail.com>
2024-08-09 17:17:54 +0900

    feat: ログイン機能周りのつなぎこみ

--- server/api/urls.py
+++ server/api/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('api-auth/', include(
         'rest_framework.urls', namespace='rest_framework')),
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
 ]
