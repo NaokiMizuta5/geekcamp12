d831ab9a9c78c7388ee968413f3ef9168e42c520
unknown <naoki.mizuta.work@gmail.com>
2024-08-11 02:42:10 +0900

    fix

--- server/server/settings.py
+++ server/server/settings.py
@@ -16,6 +16,9 @@ from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
