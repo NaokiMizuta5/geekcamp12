cd92bda1840d7eb9b3746721cfa7eee981e9e211
FkHiroki <hukuhara_0319@keio.jp>
2024-08-11 13:28:25 +0900

    settingsの修正

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'dist')],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
