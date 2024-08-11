2c9c4b79a4b3ef4828d0ec5ba8bafbd14ee748c6
unknown <naoki.mizuta.work@gmail.com>
2024-08-11 12:11:23 +0900

    update: 環境変数を丁寧に設定

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
 
 STATIC_URL = '/static/'
 
